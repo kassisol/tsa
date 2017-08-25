5d48cea3e17478ce3ca20d90361fee9c82f39734
Julien Kassar <github@kassisol.com>
2017-08-25 02:47:47 -0400

    Add new GetServerVersion function to client package

--- client/system.go
+++ client/system.go
@@ -8,7 +8,51 @@ import (
 	"github.com/juliengk/stack/client"
 	"github.com/juliengk/stack/jsonapi"
 	"github.com/kassisol/tsa/api/types"
+	"github.com/kassisol/tsa/version"
 )
+
+func (c *Config) GetServerVersion() (*version.VersionInfo, error) {
+	cc := &client.Config{
+		Scheme: c.URL.Scheme,
+		Host:   c.URL.Host,
+		Port:   c.URL.Port,
+		Path:   "/version",
+	}
+
+	req, err := client.New(cc)
+	if err != nil {
+		return nil, err
+	}
+
+	req.HeaderAdd("Accept", "application/json")
+
+	result := req.Get()
+
+	var response jsonapi.Response
+	if err := json.Unmarshal(result.Body, &response); err != nil {
+		return nil, err
+	}
+
+	if result.Response.StatusCode != 200 {
+		if response.Errors == (jsonapi.ResponseMessage{}) {
+			return nil, fmt.Errorf("Authorization denied")
+		}
+
+		return nil, fmt.Errorf(response.Errors.Message)
+	}
+
+	r, err := json.Marshal(response.Data)
+	if err != nil {
+		return nil, err
+	}
+
+	var ver version.VersionInfo
+	if err := json.Unmarshal(r, &ver); err != nil {
+		return nil, err
+	}
+
+	return &ver, nil
+}
 
 func (c *Config) GetInfo(token string) (*types.SystemInfo, error) {
 	cc := &client.Config{
