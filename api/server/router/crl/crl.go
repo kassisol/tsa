d3ea37ff0efad3dcd9c26865cf6a4221d3428908
Julien Kassar <github@kassisol.com>
2017-08-25 05:13:58 -0400

    Update crl handle

--- api/server/router/crl/crl.go
+++ api/server/router/crl/crl.go
@@ -1,14 +1,27 @@
 package crl
 
 import (
+	"net/http"
+
+	"github.com/juliengk/go-utils/filedir"
+	"github.com/juliengk/stack/jsonapi"
 	"github.com/kassisol/tsa/pkg/adf"
+	"github.com/kassisol/tsa/pkg/api"
 	"github.com/labstack/echo"
 )
 
 func CRLHandle(c echo.Context) error {
 	cfg := adf.NewDaemon()
 	if err := cfg.Init(); err != nil {
-		return err
+		r := jsonapi.NewErrorResponse(1000, err.Error())
+
+		return api.JSON(c, http.StatusInternalServerError, r)
+	}
+
+	if !filedir.FileExists(cfg.CA.CRLFile) {
+		r := jsonapi.NewErrorResponse(1000, "CRL file does not exist")
+
+		return api.JSON(c, http.StatusInternalServerError, r)
 	}
 
 	return c.File(cfg.CA.CRLFile)
