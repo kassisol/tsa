c39a3a230ecb97cc89a78a92c553e9b372cf04fa
Julien Kassar <github@kassisol.com>
2017-08-29 11:03:37 -0400

    Create daemon package

--- cmd/tsad/main.go
+++ cmd/tsad/main.go
@@ -21,49 +21,8 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/juliengk/go-utils"
 	"github.com/juliengk/go-utils/user"
-	"github.com/kassisol/tsa/pkg/adf"
-	"github.com/spf13/cobra"
+	"github.com/kassisol/tsa/daemon"
 )
-
-var (
-	serverBindAddress string
-	serverBindPort    int
-	serverTLS         bool
-	serverTLSCN       string
-	serverTLSDuration int
-	serverTLSGen      bool
-	serverTLSCert     string
-	serverTLSKey      string
-)
-
-func newCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "tsad",
-		Short: "Starts a CA (Certification Authority) server",
-		Long:  daemonDescription,
-		Run:   runDaemon,
-	}
-
-	cmd.SetHelpTemplate(helpTemplate)
-	cmd.SetUsageTemplate(usageTemplate)
-
-	cfg := adf.NewDaemon()
-	if err := cfg.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	flags := cmd.Flags()
-	flags.StringVarP(&serverBindAddress, "bind-address", "a", "0.0.0.0", "Bind Address")
-	flags.IntVarP(&serverBindPort, "bind-port", "p", 80, "Bind Port")
-	flags.BoolVarP(&serverTLS, "tls", "t", false, "Enable TLS certificates")
-	flags.StringVarP(&serverTLSCN, "tlscn", "", "", "Certificate Common Name")
-	flags.IntVarP(&serverTLSDuration, "tls-duration", "", 60, "Certificate duration")
-	flags.BoolVarP(&serverTLSGen, "tlsgen", "", false, "Auto generate self-signed TLS certificates")
-	flags.StringVarP(&serverTLSCert, "tlscert", "", cfg.API.CrtFile, "Path to TLS certificate file")
-	flags.StringVarP(&serverTLSKey, "tlskey", "", cfg.API.KeyFile, "Path to TLS key file")
-
-	return cmd
-}
 
 func main() {
 	u := user.New()
@@ -72,16 +31,15 @@ func main() {
 		log.Fatal("You must be root to run that command")
 	}
 
-	cmd := newCommand()
+	cmd := daemon.NewCommand()
+
+	cmd.SetHelpTemplate(helpTemplate)
+	cmd.SetUsageTemplate(usageTemplate)
+
 	if err := cmd.Execute(); err != nil {
 		utils.Exit(err)
 	}
 }
-
-var daemonDescription = `
-Starts a CA (Certificate Authority) server
-
-`
 
 var usageTemplate = `{{ .Short | trim }}
 
---
+++ daemon/cmd.go
@@ -0,0 +1,49 @@
+package daemon
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/kassisol/tsa/pkg/adf"
+	"github.com/spf13/cobra"
+)
+
+var (
+	serverBindAddress string
+	serverBindPort    int
+	serverTLS         bool
+	serverTLSCN       string
+	serverTLSDuration int
+	serverTLSGen      bool
+	serverTLSCert     string
+	serverTLSKey      string
+)
+
+func NewCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tsad",
+		Short: "Starts a CA (Certification Authority) server",
+		Long:  daemonDescription,
+		Run:   runDaemon,
+	}
+
+	cfg := adf.NewDaemon()
+	if err := cfg.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	flags := cmd.Flags()
+	flags.StringVarP(&serverBindAddress, "bind-address", "a", "0.0.0.0", "Bind Address")
+	flags.IntVarP(&serverBindPort, "bind-port", "p", 80, "Bind Port")
+	flags.BoolVarP(&serverTLS, "tls", "t", false, "Enable TLS certificates")
+	flags.StringVarP(&serverTLSCN, "tlscn", "", "", "Certificate Common Name")
+	flags.IntVarP(&serverTLSDuration, "tls-duration", "", 60, "Certificate duration")
+	flags.BoolVarP(&serverTLSGen, "tlsgen", "", false, "Auto generate self-signed TLS certificates")
+	flags.StringVarP(&serverTLSCert, "tlscert", "", cfg.API.CrtFile, "Path to TLS certificate file")
+	flags.StringVarP(&serverTLSKey, "tlskey", "", cfg.API.KeyFile, "Path to TLS key file")
+
+	return cmd
+}
+
+var daemonDescription = `
+Starts a CA (Certificate Authority) server
+
+`
